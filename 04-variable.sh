aa32dc9e77876a6a3400298cc70ec23e975c4787
rajani <anilkumar.guntla@gmail.com>
2023-12-14 00:13:42 +0530

    devops practise

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,3 +1,6 @@
+# paasing arguments at run time
+
+
 #! /bin/nash
 person1=$1
 person2=$2
@@ -5,3 +8,18 @@ echo "$1 : hello $2 , Good morning"
 echo "$2 :: Hey $1, How are you"
 echo "$1 : i'm good, How are $2 "
 echo "$2 : i'm good as well $1"
+
+#! /bin/nash
+person1=$1
+person2=$2
+echo "$person1 : hello $person2 , Good morning"
+echo "$person2 :: Hey $person1, How are you"
+echo "$person1 : i'm good, How are $person2 "
+echo "$person2 : i'm good as well $person1"
+
+
+#! /bin/nash
+echo "$1 : hello $2 , Good morning"
+echo "$2 :: Hey $1, How are you"
+echo "$1 : i'm good, How are $2 "
+
