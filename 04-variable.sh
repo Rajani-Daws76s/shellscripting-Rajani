a4943deb4c18112ebce0c3b1a142bc41a484342c
rajani <anilkumar.guntla@gmail.com>
2023-12-14 00:08:19 +0530

    devops practise

---
+++ 04-variable.sh
@@ -0,0 +1,7 @@
+#! /bin/nash
+person1=$1
+person2=$2
+echo "$person1 : hello $person2 , Good morning"
+echo "$person2 :: Hey $person1, How are you"
+echo "$person1 : i'm good, How are $person2 "
+echo "$person2 : i'm good as well $person1"
