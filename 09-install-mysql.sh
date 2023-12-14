45c207d13a03eff4a9d47b67a5b2d5312740594a
rajani <anilkumar.guntla@gmail.com>
2023-12-14 23:24:31 +0530

    devops-practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 ID=$(id -u) # if you run this in normal user then you will get exact o/p
+Validate()
+{
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
+else
+    echo "Installing GIT is SUCCESS"
+fi
+}
+
 if [ $ID -ne 0 ]
 then
     echo "pls run sudo user"
@@ -8,16 +19,6 @@ else
 echo "your running with sudo user"
 fi
 yum install mysql -y
-if [ $? -ne 0 ]
-then 
-echo "Error: MySQl installation failed"
-else
-echo "MYSQl installation success"
-fi
-if [ $? -ne 0 ]
-then
-    echo "ERROR:: Installing GIT is failed"
-    exit 1
-else
-    echo "Installing GIT is SUCCESS"
-fi
+validate
+yum install git -y
+validate
