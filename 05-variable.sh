5ca493ed169d09059613ee7ed3b2de7d3a98856e
rajani <anilkumar.guntla@gmail.com>
2023-12-14 20:58:30 +0530

    devops-practice

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,4 +1,19 @@
+
 #! /bin/bash
 USERNAME=$1
 PASSWORD=$2
 echo "Username : ${USERNAME} , Password : ${PASSWORD}"
+
+*************
+
+#! /bin/bash
+
+echo "Pls enter USERNAME"
+read USERNAME #user name will displayed at runtime
+echo "username is $USERNAME"
+
+echo "Pls enter USERNAME"
+read -s USERNAME #user name will displayed at runtime
+echo "username is $USERNAME"
+
+
