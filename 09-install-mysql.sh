6083555d1137b21ecc660f8f2210a850d35d655f
rajani <anilkumar.guntla@gmail.com>
2023-12-14 23:07:57 +0530

    devops-practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -3,6 +3,7 @@ ID=$(id -u) # if you run this in normal user then you will get exact o/p
 if [ $ID -ne 0 ]
 then
     echo "pls run sudo user"
+    exit 1 # you can give any number other 0
 else
 echo "your running with sudo user"
 fi
@@ -12,4 +13,11 @@ then
 echo "Error: MySQl installation failed"
 else
 echo "MYSQl installation success"
+fi
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
+else
+    echo "Installing GIT is SUCCESS"
 fi
