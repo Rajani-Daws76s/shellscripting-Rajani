68d43cb7ae6ec923c1690597aaac71d43201f3f0
rajani <anilkumar.guntla@gmail.com>
2023-12-14 23:04:50 +0530

    devops-practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-ID=$(id -u)
+ID=$(id -u) # if you run this in normal user then you will get exact o/p
 if [ $ID -ne 0 ]
 then
     echo "pls run sudo user"
 else
 echo "your running with sudo user"
+fi
+yum install mysql -y
+if [ $? -ne 0]
+then 
+echo "Error: MySQl installation failed"
+else
+echo "MYSQl installation success"
 fi
