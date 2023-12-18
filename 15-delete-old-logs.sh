7de12c2211856632cecc7eb148eef2dec86957b8
rajani <anilkumar.guntla@gmail.com>
2023-12-18 22:56:52 +0530

    devops practice

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SOURCE-DIR=/tmp/shell-scripting-logs
+SOURCE-DIR="/tmp/shell-scripting-logs"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -7,7 +7,7 @@ N="\e[0m"
 #to check whether source directory exist or not
 if [ ! -d $SOURCE-DIR ] # ! denotes opposite
 then 
-echo "$R Source dirctory : $SOURCE-DIR does not exist"
+echo "$R Source dirctory : $SOURCE-DIR does not exist $N"
 fi 
 Files-to-delete=find $SOURCE-DIR - type f -mtime +14 -name "*.log"
 while IFS= read -r line
