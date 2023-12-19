db49a1f31ae204cd9176884e912a7e6317878698
Rajani-Daws76s <152277581+Rajani-Daws76s@users.noreply.github.com>
2023-12-19 16:26:18 +0530

    Update 17-disk-usage.sh

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DISK-USAGE=$(df -hT | grep -vE '\tmp|File')
-DISK-Threshold=1
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
 Message=""
 while IFS=read line
 do 
 Usage=$(echo $line | awk '{print $6f}' | cut -d % -f1)
 Partition=$(echo $line | awk '{print $1f}')
-if [ Usage -ge DISK-Threshold ]
+if [ Usage -ge DISK_THRESHOLD ]
 then
 Message="High disk usage on $Partition:$Usage \n"
 fi
