b9bf37c8bc54a00da2b4bd23b9774c939d9afa63
rajani <anilkumar.guntla@gmail.com>
2023-12-19 16:14:13 +0530

    devops practice

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -3,13 +3,13 @@ DISK-USAGE=$(df -hT | grep -vE '\tmp|file')
 DISK-Threshold=1
 Message=""
 
-while IFS=read -r line
+while IFS=read line
 do 
 Usage=$(echo $line | awk '{print $6f}' | cut -d % -f1)
 Partition=$(echo $line | awk '{print $1f}')
 if [ Usage -ge DISK-Threshold ]
 then
-Message="High disk usage on $Partition:$Partition \n"
+Message="High disk usage on $Partition:$Usage \n"
 fi
 done<<<$DISK-USAGE
 echo -e "message: $Message"
